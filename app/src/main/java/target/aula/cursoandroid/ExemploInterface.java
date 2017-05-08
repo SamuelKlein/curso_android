948c927ca3a6f74d08560c027742976793e61522
Samuel A. Klein <samuel3dstudio@gmail.com>
2017-05-08 12:04:49 -0300

    primeiro layout

---
+++ app/src/main/java/target/aula/cursoandroid/ExemploInterface.java
@@ -0,0 +1,13 @@
+package target.aula.cursoandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExemploInterface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exemplo_interface);
+    }
+}

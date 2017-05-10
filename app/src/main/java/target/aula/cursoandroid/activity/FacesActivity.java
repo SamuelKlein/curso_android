0a5d5a39496805be4380ead3801f61518d84bf87
Samuel A. Klein <samuel3dstudio@gmail.com>
2017-05-10 09:33:20 -0300

    testes

---
+++ app/src/main/java/target/aula/cursoandroid/activity/FacesActivity.java
@@ -0,0 +1,15 @@
+package target.aula.cursoandroid.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import target.aula.cursoandroid.R;
+
+public class FacesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faces);
+    }
+}

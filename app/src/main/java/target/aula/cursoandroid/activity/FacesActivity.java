5c2c638e055ad1f391d4128d5dfcf583ce99a3bc
Samuel A. Klein <samuel3dstudio@gmail.com>
2017-05-11 12:02:24 -0300

    libs

--- app/src/main/java/target/aula/cursoandroid/activity/FacesActivity.java
+++ app/src/main/java/target/aula/cursoandroid/activity/FacesActivity.java
@@ -2,6 +2,9 @@ package target.aula.cursoandroid.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.afollestad.materialdialogs.MaterialDialog;
 
 import target.aula.cursoandroid.R;
 
@@ -11,7 +14,39 @@ public class FacesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faces);
+    }
 
+    public void dialogtext(View view) {
 
+        MaterialDialog dialog = new MaterialDialog.Builder(this)
+                .title(R.string.title)
+                .content(R.string.content)
+                .positiveText(R.string.agree)
+                .show();
+
+    }
+
+    public void dialogList(View view) {
+
+        new MaterialDialog.Builder(this)
+                .title(R.string.title)
+                .items(R.array.minhaLista)
+                .itemsCallback(new MaterialDialog.ListCallback() {
+                    @Override
+                    public void onSelection(MaterialDialog dialog, View view, int which, CharSequence text) {
+                    }
+                })
+                .show();
+
+    }
+
+    public void dialogView(View view) {
+
+        boolean wrapInScrollView = true;
+        new MaterialDialog.Builder(this)
+                .title(R.string.title)
+                .customView(R.layout.dialog_loko, wrapInScrollView)
+                .positiveText(R.string.positive)
+                .show();
     }
 }
